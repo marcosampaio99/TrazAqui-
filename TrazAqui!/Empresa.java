de0054c8be149b3518a5223090066e97a87dde8b
marcosampaio99 <59233990+marcosampaio99@users.noreply.github.com>
2020-04-14 01:23:21 +0100

    Add files via upload

---
+++ TrazAqui!/Empresa.java
@@ -0,0 +1,160 @@
+
+/**
+ * Escreva a descrição da classe Empresa aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class Empresa extends Utilizador
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int raiogeografico;
+    private int velocidade;
+    private int rating;
+    private int taxa; // em percentagem
+    private int multitasking;//1 se fizer mais do q 1 entrega ao mesmo tempo 0 se nao
+    private int indicador; //1 se estiver disposto a recolher 0 se nao
+    private int capacidade; // quantas encomendas conseguem tratar ao mm tempo 
+    // lista de entregas efetuadas;
+
+    /**
+     * COnstrutor para objetos da classe Empresa
+     */
+    public Empresa()
+    {
+        // inicializa variáveis de instância
+        super();
+        this.raiogeografico=0;
+        this.velocidade=0;
+        this.rating=0;
+        this.taxa=0;
+        this.multitasking=0;
+        this.indicador=1;
+        this.capacidade=1;
+    }
+
+    public Empresa(Empresa e){
+        super(e);
+        this.raiogeografico=e.getRaiogeografico();
+        this.velocidade=e.getVelocidade();
+        this.rating=e.getRating();
+        this.taxa=e.getTaxa();
+        this.multitasking=e.getMulti();
+        this.indicador=e.getIndicador();
+        this.capacidade=e.getCapacidade();
+    }
+    
+    public Empresa(String nome,String email,String password,Localizacao localizacao, int raiogeografico, int velocidade, int rating, int taxa,int multitasking, int indicador, int capacidade){
+        super(nome,email,password,localizacao);
+        this.raiogeografico=raiogeografico;
+        this.velocidade=velocidade;
+        this.rating=rating;
+        this.taxa=taxa;
+        this.multitasking=multitasking;
+        this.indicador=indicador;
+        this.capacidade=capacidade;
+    }
+    
+    
+    // getters 
+    
+    public int getRaiogeografico(){
+        return this.raiogeografico;
+    }
+    
+    public int getVelocidade(){
+        return this.velocidade;
+    }
+    
+    public int getRating(){
+        return this.rating;
+    }
+    
+    public int getTaxa(){
+        return this.taxa;
+    }
+    
+
+    public int getMulti(){
+        return this.multitasking;
+    }
+    
+    
+    public int getIndicador(){
+        return this.indicador;
+    }
+    
+    
+    public int getCapacidade(){
+        return this.capacidade;
+    }
+    
+    // setters
+    
+    public void setRaiogeografico(){
+        this.raiogeografico=raiogeografico;
+    }
+    
+    public void setVelocidade(){
+        this.velocidade=velocidade;
+    }
+    
+    public void setRating(){
+        this.rating=rating;
+    }
+    
+     public void setTaxa(){
+        this.taxa=taxa;
+    }
+    
+
+     public void setMulti(){
+        this.multitasking=multitasking;
+    }
+    
+    public void setIndicador(){
+    this.indicador=indicador;
+    }
+    
+     public void setCapacidade(){
+        this.capacidade=capacidade;
+    }
+    
+    public boolean equals (Object o){
+        if(this==o) return true;
+        
+        if((o==null) || (this.getClass()!=o.getClass())) return false;
+        
+        Empresa e = (Empresa) o;
+        return(super.equals(e)&&this.raiogeografico==e.getRaiogeografico())
+            &&(this.velocidade==e.getVelocidade())
+            &&(this.rating==e.getRating())&&(this.taxa==e.getTaxa())
+            &&(this.multitasking==e.getMulti()&&(this.indicador==e.getIndicador())
+            &&(this.capacidade==e.getCapacidade()));
+            
+    }
+    
+    public String toString() {
+        String s = new String();
+ 
+        s = ("Empresa: \n" + 
+               "Nome: " + this.getNome() + "\n" + 
+               "Password: " + this.getPassword() + "\n" +
+               "Email: " + this.getEmail() + "\n")+
+               "Raio Geografico(m): " + this.getRaiogeografico() + "\n" +
+               "Velocidade média (m/s): " + this.velocidade +"\n" +
+               "Rating: " + this.rating + "\n" +
+               "Multitasking(Sim:1/Não:0): " + this.multitasking + "\n" +
+               "Disponível(Sim:1/Não:0): " + this.indicador + "\n" +
+               "Pedidos disponíveis ao mesmo tempo: " + this.capacidade +  "\n";
+               System.out.println(this.getLocalizacao());
+ 
+        return s;
+    }
+    
+    
+    public Empresa clone() {
+        return new Empresa(this);
+    }
+    
+}
--- TrazAqui!/Teste.java
+++ TrazAqui!/Teste.java
@@ -4,8 +4,11 @@ public class Teste{
        
         Voluntario voluntario1= new Voluntario("Vasquinho","v1@mail.com","123456",localizacao1,5000,50,0);
         Cliente cliente1 = new Cliente("Pablito","u1@mail.com","123456",localizacao1);
-
+        Empresa empresa1 = new Empresa("Ideia Fixe","e1@mail.com","1234",localizacao1,5000,60,0,3,0,1,1);
+        Empresa empresa2 = new Empresa("Ideia Má","e2@mail.com","12345",localizacao1,15000,55,0,4,1,1,30);
         System.out.println(cliente1);
         System.out.println(voluntario1);
+        System.out.println(empresa1);
+        System.out.println(empresa2);
     }
 }
